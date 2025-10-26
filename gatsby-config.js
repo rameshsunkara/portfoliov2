2268dc619808bb32a5c4d2d865898afa043f8b53
Ramesh Sunkara <rsunkara@Rameshs-Mac-mini.local>
2025-10-25 22:37:51 -0400

    works with latest node

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,20 @@
 const config = require('./src/config');
 
 module.exports = {
+  pathPrefix: '/',
+  flags: {
+    DEV_SSR: false,
+    FAST_DEV: false,
+    PRESERVE_FILE_DOWNLOAD_CACHE: false,
+    PARALLEL_SOURCING: false,
+  },
   siteMetadata: {
     title: 'Ramesh Sunkara',
-    description: 'Ramesh Sunkara is an Enterprise Software Architect based in Raleigh, NC',
-    siteUrl: 'https://rsunkara.com', // No trailing slash allowed!
+    description:
+      'Ramesh Sunkara is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://rameshsunkara.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@rameshtalks',
+    twitterUsername: '@rameshsunkara',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
